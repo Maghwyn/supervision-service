498246e0e3b4119ec65cb83566060f6e7975311e
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-05-26 02:55:24 +0200

    fix(sh): Fix the path of the file in the vitals service installer

--- linux_daemon_installer.sh
+++ linux_daemon_installer.sh
@@ -8,35 +8,35 @@ fi
 sudo -v &> /dev/null
 
 file_name="vitals";
-system_path="/etc/systemd/sytem/";
+system_path="/etc/systemd/system/";
 sudo touch $system_path$file_name".service";
 
-echo "[Unit]" > $file_name.service;
-echo "Description=System Vitals Service" >> $file_name.service;
+echo "[Unit]" >> $system_path$file_name.service;
+echo "Description=System Vitals Service" >> $system_path$file_name.service;
 
-echo "" >> $file_name.service;
+echo "" >> $system_path$file_name.service;
 
-echo "[Service]" >> $file_name.service;
+echo "[Service]" >> $system_path$file_name.service;
 echo "Which user account should the service be run on ?";
 read user;
-echo "User="$user >> $file_name.service;
+echo "User="$user >> $system_path$file_name.service;
 
 echo "Please enter your working directory absolute path :";
 read directory;
-echo "WorkingDirectory="$directory >> $file_name.service;
+echo "WorkingDirectory="$directory >> $system_path$file_name.service;
 echo "Please enter your python execution path relative to the directory path :";
 read python;
-echo "ExecStart="$python" main.py" >> $file_name.service;
+echo "ExecStart="$python" main.py" >> $system_path$file_name.service;
 echo "Should this service always restart ?(Y/N)";
 read answer;
 if [[ $answer == "Y" || $answer == "y" ]]; then
-	echo "Restart=always" >> $file_name.service;
+	echo "Restart=always" >> $system_path$file_name.service;
 fi
 
-echo "" >> $file_name.service;
+echo "" >> $system_path$file_name.service;
 
-echo "[Install]" >> $file_name.service;
-echo "WantedBy=multi-user.target" >> $file_name.service;
+echo "[Install]" >> $system_path$file_name.service;
+echo "WantedBy=multi-user.target" >> $system_path$file_name.service;
 
 echo "Your service was succesfully created at" $system_path$file_name".service";
 echo "You can start it right away, or check by yourself if the service is correctly setup and run the command manually";
